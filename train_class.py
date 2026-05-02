cec268ec35e79c43194673944485354eb91dc45e
Yuchen Wang <wangyuch@umich.edu>
2026-05-02 00:29:59 -0400

    train class setup functions clean up

--- train_class.py
+++ train_class.py
@@ -71,23 +71,36 @@ class Trainer:
         pass
 
     def setup_distributed(self):
-        """
-        various inits
+        # torchrun --nproc_per_node=4 train.py
+        # Process 0: RANK=0, LOCAL_RANK=0, WORLD_SIZE=4
+        # Process 1: RANK=1, LOCAL_RANK=1, WORLD_SIZE=4
+        # Process 2: RANK=2, LOCAL_RANK=2, WORLD_SIZE=4
+        # Process 3: RANK=3, LOCAL_RANK=3, WORLD_SIZE=4
 
-        TODO: FIGURE OUT WHAT THIS IS DOING AND RETYPE
-        """
+        # NCCL — NVIDIA's library, optimized for GPU-to-GPU transfers over NVLink or InfiniBand. Always use this for GPU training
         
+        # distributed data parallelism
         self.ddp = int(os.environ.get("RANK", -1)) != -1
         if self.ddp:
+            # setup handshake, so processes have communication channels
             init_process_group(backend=self.args.backend)
+            # RANK: global process ID across all machines. If you have 2 machines × 4 GPUs, ranks are 0–7
             self.rank = int(os.environ["RANK"])
+            # LOCAL_RANK: process ID within a single machine. Used to assign which GPU this process owns.
             self.local_rank = int(os.environ["LOCAL_RANK"])
+            # WORLD_SIZE: total number of processes
             self.world_size = int(os.environ["WORLD_SIZE"])
+
             self.device = f"cuda:{self.local_rank}"
+
             torch.cuda.set_device(self.device)
             self.master_process = self.rank == 0
+            # each process gets a different random seed so dropout masks and any stochasticity differs across GPUs
             self.seed_offset = self.rank
 
+            # Gradient Accumulation: simulate higher batch size
+            #   do multiple forward/backward passes and accumulate gradients before doing one optimizer step
+            # Accumulation is distributed, so needs to divide evently amongst GPUs
             assert self.args.gradient_accumulation_steps % self.world_size == 0
             self.grad_accum_steps = self.args.gradient_accumulation_steps // self.world_size
         else:
@@ -99,6 +112,7 @@ class Trainer:
             self.seed_offset = 0
             self.grad_accum_steps = self.args.gradient_accumulation_steps
 
+        self.device_type = 'cuda' if 'cuda' in self.device else 'cpu'
         self.tokens_per_iter = (
             self.grad_accum_steps * self.world_size * self.args.batch_size * self.args.block_size
         )
@@ -106,8 +120,6 @@ class Trainer:
     def setup_runtime(self):
         """
         sets up the backend runtime based on varaiables in setup distributed
-        
-        TODO: FIGURE OUT WHAT THIS IS DOING AND RETYPE
         """
         
         if self.master_process:
@@ -115,10 +127,14 @@ class Trainer:
         torch.manual_seed(1337 + self.seed_offset)
         torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
         torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-        device_type = 'cuda' if 'cuda' in self.device else 'cpu' # for later use in torch.autocast
-        # note: float16 data type will automatically use a GradScaler
-        ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[self.dtype]
-        ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+
+        ctx = nullcontext()
+        if self.device_type == 'gpu':
+            # note: float16 data type will automatically use a GradScaler
+            # https://docs.pytorch.org/docs/2.11/amp.html -> AMP TORCH DOCS
+            ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[self.dtype]
+            ctx = torch.amp.autocast(device_type=self.device_type, dtype=ptdtype)
+
 
     def cleanup(self):
         if self.ddp:
